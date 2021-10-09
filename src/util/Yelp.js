09789fb7f7eb027dcbe7067b1a9fb0cffe551bda
Osborne92 <danechristensen92@gmail.com>
2021-10-09 16:24:12 -0400

    comments

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -3,7 +3,7 @@ const apiKey = ''
 const Yelp = {
     search(term, location, sortBy) { // retrieves search results from api
         return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
-            headers: {
+            headers: { // API can be found at https://www.yelp.com/developers/documentation/v3/get_started
                 Authorization: `Bearer ${apiKey}`
             }
         }).then(response => {
